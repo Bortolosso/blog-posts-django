5a6c419567ac7a5ab9130196061d3946d3075ca5
joao_jb <joao.bortolosso@fcamara.com.br>
2020-01-14 17:40:52 -0300

    added form new post and new page index

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('list', views.PostListAll.as_view(), name='list_post'),
-    url(r'^register/$',views.register,name='register'),
-    url(r'^user_login/$',views.user_login,name='user_login'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    url(r'^register/$', views.register, name = 'register'),
+    url(r'^login/$', views.user_login, name = 'login'),
+    url(r'^addedpost/$', views.added_post, name = 'added_post'),
+    path('<slug:slug>/', views.post_detail, name = 'post_detail')
 ]
